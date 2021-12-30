5d06be4f9e91ed84897be980646c5892d1a1b896
vsl700 <51147745+vsl700@users.noreply.github.com>
2021-12-30 18:08:48 +0200

    Added the two lists, need to add functionality

---
+++ wwwroot/js/track-lists.js
@@ -0,0 +1,18 @@
+﻿var locList = false;
+var phonesList = document.getElementById("phonesList");
+var locsList = document.getElementById("locsList");
+
+function list_click() {
+    alert("CLICK");
+    if (locList) {
+        phonesList.style.display = "block";
+        locsList.style.display = "none";
+    } else {
+        locList.style.display = "block";
+        phonesList.style.display = "none";
+    }
+
+    locList = !locList;
+}
+
+phonesList.style.display = "none";
