93bda27806bd673f4ec8757db695066414c4359b
vsl700 <51147745+vsl700@users.noreply.github.com>
2022-02-04 14:57:46 +0200

    Added on-track notificator in the hub (Caller->Target)

--- Hubs/NotificationUserHub.cs
+++ Hubs/NotificationUserHub.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using PhoneTracker.Models;
 using PhoneTrackerOnline.Interface;
 using System;
 using System.Collections.Generic;
@@ -10,15 +11,24 @@ namespace PhoneTrackerOnline.Hubs
     public class NotificationUserHub : Hub
     {
         private readonly IUserConnectionManager _userConnectionManager;
+        private readonly ApplicationDbContext _db;
 
-        public NotificationUserHub(IUserConnectionManager userConnectionManager)
+        public NotificationUserHub(IUserConnectionManager userConnectionManager, ApplicationDbContext db)
         {
             _userConnectionManager = userConnectionManager;
+            _db = db;
         }
 
         public override Task OnConnectedAsync()
         {
             GetConnectionId();
+
+            string userId = Context.GetHttpContext().Request.Query["userId"];
+            if (IsACaller(userId))
+            {
+                SendTrackRequest(userId, "trackin");
+            }
+            
 
             return base.OnConnectedAsync();
         }
@@ -36,7 +46,43 @@ namespace PhoneTrackerOnline.Hubs
             //get the connectionId
             var connectionId = Context.ConnectionId;
             _userConnectionManager.RemoveUserConnection(connectionId);
+
+            string userId = Context.GetHttpContext().Request.Query["userId"];
+            if (IsACaller(userId))
+            {
+                SendTrackRequest(userId, "no-trackin");
+            }
+
             var value = await Task.FromResult(0);
+        }
+
+        private void SendTrackRequest(string userId, string message)
+        {
+            foreach (var phone in _db.TargetPhones)
+            {
+                var user = _db.CallerUsers.Find(phone.UserID);
+                if (user != null && user.Username == userId)
+                {
+                    var connections = _userConnectionManager.GetUserConnections(phone.Code + "");
+                    if (connections != null && connections.Count > 0)
+                    {
+                        Clients.Client(connections[0]).SendAsync("sendToTarget", message);
+                    }
+                }
+            }
+
+            
+        }
+
+        private bool IsACaller(string userId)
+        {
+            foreach(char ch in userId)
+            {
+                if (char.IsLetter(ch))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
