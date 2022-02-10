9584424d19aab8d0d0f41f69b55256657b072188
vsl700 <51147745+vsl700@users.noreply.github.com>
2022-02-10 23:20:25 +0200

    Fixed a warning and re-published

--- wwwroot/phonetrackeronline/wwwroot/js/notification.js
+++ wwwroot/phonetrackeronline/wwwroot/js/notification.js
@@ -1,19 +1,31 @@
-﻿var connection = new signalR.HubConnectionBuilder().withUrl("/NotificationUserHub?userId=" + userId).build();
-connection.on("sendToUser", (articleHeading, articleContent) => {
-    var heading = document.createElement("h3");
-    heading.textContent = articleHeading;
-    var p = document.createElement("p");
-    p.innerText = articleContent;
-    var div = document.createElement("div");
-    div.appendChild(heading);
-    div.appendChild(p);
-    document.getElementById("locsList").appendChild(div);
+﻿// Set up the state notificators
+var onlineDescs = document.getElementsByClassName("online-desc");
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/NotificationUserHub?userId=" + userId).build();
+connection.on("sendToUser", (targetCode, value) => {
+    for (i = 0; i < onlineDescs.length; i++) {
+        let index = i;
+        if (onlineDescs[index].id == targetCode) {
+            onlineDescs[index].style = null; // null because otherwise it sets the display to block, which separates the text from the name, while that way it resets the style as if it has never been set, which is the working way in the case
+
+            let location = JSON.parse(value);
+            let tempLat = location[0];
+            let tempLng = location[1];
+
+            if (currentLocationMarkers[index] != null)
+                currentLocationMarkers[index].setMap(null);
+
+            CreateMarker(tempLat, tempLng, "phone_loc_current.png");
+            currentLocationMarkers[index] = marker; 
+
+            break;
+        }
+    }
 });
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
 }).then(function () {
-    //document.getElementById("user").innerHTML = "UserId: " + userId;
     connection.invoke('GetConnectionId').then(function (connectionId) {
         document.getElementById('signalRConnectionId').innerHTML = connectionId;
     })
--- wwwroot/phonetrackeronline/wwwroot/js/track-lists.js
+++ wwwroot/phonetrackeronline/wwwroot/js/track-lists.js
@@ -1,26 +1,56 @@
-﻿var phonesList = document.getElementById("phonesList");
-var locsList = document.getElementById("locsList");
-//var popup = document.getElementById("popup");
+﻿var currentId = 0; // To determine which phone is currently chosen
+let selectedPhoneElement = null;
+let selectedLocElement = null;
+
+var phonesList = document.getElementById("phonesList");
+var locsList = document.getElementById("locsList-0");
+var locationMarkers;
+var currentLocationMarkers = [];
 
 function locsList_click() {
-    locsList.style.display = "block";
+    let currentLocsList = document.getElementById("locsList-" + currentId);
+    let buttons = currentLocsList.getElementsByClassName("loc-select");
+    locationMarkers = [];
+    for (i = 0; i < buttons.length; i++) {
+        let index = i;
+
+        // Placing markers...
+        let tempLat = buttons[index].getElementsByClassName("lat")[0].textContent.replace(",", ".");
+        tempLat = parseFloat(tempLat);
+        let tempLng = buttons[index].getElementsByClassName("lng")[0].textContent.replace(",", ".");
+        tempLng = parseFloat(tempLng);
+
+        let tempImageSrc = buttons[index].getElementsByClassName("loc-icon")[0].src;
+        
+        CreateMarker(tempLat, tempLng, tempImageSrc);
+        locationMarkers[index] = marker;
+    }
+
+    currentLocsList.style.display = "block";
     phonesList.style.display = "none";
     locsListBtn.style.display = "none";
     phonesListBtn.style.display = "block";
 }
 
 function phonesList_click() {
-    locsList.style.display = "none";
+    if (selectedLocElement != null) {
+        selectedLocElement.style.backgroundColor = null;
+        selectedLocElement = null;
+    }
+
+    let currentLocsList = document.getElementById("locsList-" + currentId);
+    if (locationMarkers != null) {
+        let buttons = currentLocsList.getElementsByClassName("loc-select");
+        for (i = 0; i < buttons.length; i++) {
+            locationMarkers[i].setMap(null);
+        }
+    }
+
+    currentLocsList.style.display = "none";
     phonesList.style.display = "block";
     locsListBtn.style.display = "block";
     phonesListBtn.style.display = "none";
-    //popup.style.display = "none";
 }
-
-/*function phonesList_add() {
-    //phonesList.style.display = "none";
-    //popup.style.display = "block";
-}*/
 
 var locsListBtn = document.getElementById("locsListBtn");
 var phonesListBtn = document.getElementById("phonesListBtn");
@@ -30,5 +60,45 @@ phonesListBtn.addEventListener("click", () => { phonesList_click(); })
 
 phonesList_click();
 
-//var addBtn = document.getElementById("addBtn");
-//addBtn.addEventListener("click", () => { phonesList_add(); });
+
+var phoneButtons = document.getElementsByClassName("phone-select");
+for (i = 0; i < phoneButtons.length; i++) {
+    let index = i;
+    phoneButtons[index].onclick = function () {
+        if (selectedPhoneElement != null) {
+            selectedPhoneElement.style.backgroundColor = null;
+        }
+
+        var id = phoneButtons[index].id;
+        currentId = parseInt(id.substring(id.lastIndexOf("-") + 1));
+
+        selectedPhoneElement = phoneButtons[index];
+        selectedPhoneElement.style.backgroundColor = "lightgray";
+
+        if (currentLocationMarkers[index] != null)
+            MoveCamera(currentLocationMarkers[index].getPosition().lat(), currentLocationMarkers[index].getPosition().lng());
+    }
+}
+
+var locsLists = document.getElementsByClassName("locsList");
+for (i = 0; i < locsLists.length; i++) {
+    let buttons = locsLists[i].getElementsByClassName("loc-select");
+    for (j = 0; j < buttons.length; j++) {
+        let index = j;
+        buttons[index].onclick = function () {
+            if (selectedLocElement != null) {
+                selectedLocElement.style.backgroundColor = null;
+            }
+
+            selectedLocElement = buttons[index];
+            selectedLocElement.style.backgroundColor = "lightgray";
+
+            let tempLat = buttons[index].getElementsByClassName("lat")[0].textContent.replace(",", ".");
+            tempLat = parseFloat(tempLat);
+            let tempLng = buttons[index].getElementsByClassName("lng")[0].textContent.replace(",", ".");
+            tempLng = parseFloat(tempLng);
+
+            MoveCamera(tempLat, tempLng);
+        }
+    }
+}
