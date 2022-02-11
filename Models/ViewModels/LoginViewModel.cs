d45b0704887a2af0cfb56529fdd4751dfe69237d
vsl700 <51147745+vsl700@users.noreply.github.com>
2022-02-11 12:29:41 +0200

    Re-configured Login/Register mechanisms

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PhoneTracker.Models.ViewModels
 {
@@ -13,7 +9,6 @@ namespace PhoneTracker.Models.ViewModels
 
         [Required]
         [DataType(DataType.Password)]
-        //[RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "The Password must contain at least one upper or lower case letter and one number, and no symbols")]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
--- Models/ViewModels/RegisterViewModel.cs
+++ Models/ViewModels/RegisterViewModel.cs
@@ -11,7 +11,7 @@ namespace PhoneTracker.Models.ViewModels
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
-        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$", ErrorMessage = "The Password must contain at least one upper or lower case letter and one number, and no symbols")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "Confirm Password")]
