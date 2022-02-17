fbdffb26f7d0bdbffa83e37560ff310ef0500599
vsl700 <51147745+vsl700@users.noreply.github.com>
2022-02-17 20:32:00 +0200

    Added some SSL Validation API for punchsalad.com

---
+++ Controllers/SSLValidationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneTrackerOnline.Controllers
+{
+    [Route(".well-known/acme-challenge")]
+    [ApiController]
+    public class SSLValidationController : ControllerBase
+    {
+        [HttpGet("nIseS2eVZqmEmaWr7ygSkbdLpP83UZgk_WmbhLhH4MQ")]
+        public string GetCode()
+        {
+            return "nIseS2eVZqmEmaWr7ygSkbdLpP83UZgk_WmbhLhH4MQ.sVPdObX68kRGTdgM3t5nkIWyVY2N2qigZ4_AM4Zcj6w";
+        }
+    }
+}
